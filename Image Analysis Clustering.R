b58ec964651a3de89dc60d18aece4d60ff8ee2e4
vz415 <vdzaball@gmail.com>
2016-09-21 18:56:52 +0100

    Whew. After a couple of crashes, see the value here.

--- Image Analysis Clustering.R
+++ Image Analysis Clustering.R
@@ -18,5 +18,16 @@ tapply(flowerVector, flowerClusters, mean)
 dim(flowerClusters) <- c(50,50) # 50x50 resolution matrix
 image(flowerClusters, axes = F)
 image(flowerMatrix, axes = F, col = grey(seq(0,1,length = 256))) # length of 256 for greyscale, plus 0-1 for white to black.
+# hierarchical clustering of MRI data
+healthy <- read.csv("healthy.csv", header = F)
+healthyMatrix <- as.matrix(healthy)
+str(healthyMatrix)
+image(healthyMatrix, axes = F, col= grey(seq(0,1,length = 256)))
+healthyVector <- as.vector(healthyMatrix)
+# distance <- dist(healthyVector, method = "euclidean") -----> don't do this! 2GB of data!
+str(healthyVector)
+n <- 365636
+n*(n-1)/2
+# let's do k means instead
 
 
