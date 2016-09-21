500864813ea4a4a1279c0b5dd19e274fa68ff144
vz415 <vdzaball@gmail.com>
2016-09-21 18:18:20 +0100

    analyzing flower data. created distances.

--- Image Analysis Clustering.R
+++ Image Analysis Clustering.R
@@ -1,3 +1,11 @@
 # Clustering image information to get insights. Coo..
 # Loading it up.
+flower <- read.csv("flower.csv", header = F)
+str(flower)
+flowerMatrix <- as.matrix(flower)
+str(flowerMatrix)
+flowerVector <- as.vector(flowerMatrix)
+str(flowerVector)
+# Calc distances
+distance <- dist(flowerVector, method = "euclidean")
 
