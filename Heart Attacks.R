a65d74a36a1435bf2d5f78cc819d2b77ac9e9c47
vz415 <vdzaball@gmail.com>
2016-09-21 18:01:31 +0100

    initial commits.

---
+++ Heart Attacks.R
@@ -0,0 +1,4 @@
+# heart attack clustering notes
+# Can use Random Forest to predict whether has a heart attack for each of the cost buckets using the random forest algorithm.
+# Looks like the random forests help eachother to make better clustering predictions. 
+# Just notes.
