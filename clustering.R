7a9f2acd54596d809e121547100d6625566795c6
vz415 <vdzaball@gmail.com>
2016-09-22 09:23:27 +0100

    Finished up this clustering exercise.

--- clustering.R
+++ clustering.R
@@ -45,25 +45,28 @@ wssplot(wine)
 # segment of clusters (i.e. 1 to 15). The sum of squares is the distance from the center point to surrounding data points. Makes sense, 
 # because the distance will drop steeply with mroe center points, but when a certain saturation point has been reached, improvements will slow.
 
+
+
+
 # Method 2: Use the NbClust library, which runs many experiments
 # and gives a distribution of potential number of clusters.
 
 library(NbClust)
 set.seed(1234)
-nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
+nc <- NbClust(wine, min.nc=2, max.nc=15, method="kmeans")
 barplot(table(nc$Best.n[1,]),
 	          xlab="Numer of Clusters", ylab="Number of Criteria",
 		            main="Number of Clusters Chosen by 26 Criteria")
 
 
 # Exercise 3: How many clusters does this method suggest?
-
+# Answer: Among all indices, 15 proposed 3 clusters as the best number of clusters
 
 # Exercise 4: Once you've picked the number of clusters, run k-means 
 # using this number of clusters. Output the result of calling kmeans()
 # into a variable fit.km
 
-# fit.km <- kmeans( ... )
+fit.km <- kmeans(wine, centers = 3)
 
 # Now we want to evaluate how well this clustering does.
 
@@ -71,9 +74,18 @@ barplot(table(nc$Best.n[1,]),
 # compares to the actual wine types in wine$Type. Would you consider this a good
 # clustering?
 
+table(fit.km$cluster)
+# Was asked to delete wine$Type earlier, but after rechecking the variable, yup, 3 clusters matches original dataset.
 
 # Exercise 6:
 # * Visualize these clusters using  function clusplot() from the cluster library
 # * Would you consider this a good clustering?
 
-#clusplot( ... )
+clusplot(wine, fit.km$cluster, color = T, shade = T)
+
+# Looks good to me!Just to check, let's see what 4 clusters would do:
+fit.km <- kmeans(wine, centers = 4)
+clusplot(wine, fit.km$cluster, color = T, shade = T)
+# with 4 clusters there is way more overlap between the groups than before. Stick with 3. How do we quantify this overlap?
+
+
